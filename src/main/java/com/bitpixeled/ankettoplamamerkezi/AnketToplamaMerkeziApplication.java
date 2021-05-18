224432ef9c55a101ac63d55a6f21b1ff20e45d61
bitpixeled <bitpixeled@gmail.com>
2021-05-18 05:05:20 +0300

    Initial commit

---
+++ src/main/java/com/bitpixeled/ankettoplamamerkezi/AnketToplamaMerkeziApplication.java
@@ -0,0 +1,13 @@
+package com.bitpixeled.ankettoplamamerkezi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnketToplamaMerkeziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnketToplamaMerkeziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitpixeled/ankettoplamamerkezi/AnketToplamaMerkeziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitpixeled.ankettoplamamerkezi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnketToplamaMerkeziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
